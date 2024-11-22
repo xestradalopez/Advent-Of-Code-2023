4b461489f983cd9435241c056f7c0743b26817b2
xestradalopez <pokelover097@gmail.com>
2024-11-22 14:51:15 -0700

    Mostly done cleaning up day 20, may come back later

--- 2023/java/day20.java
+++ 2023/java/day20.java
@@ -1,10 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
-@SuppressWarnings("SequencedCollectionMethodCanBeUsed")
+
 public class day20
 {
     static int lowPulses = 0;
@@ -19,9 +18,6 @@ public class day20
         int partOne = partOne(input);
         int partTwo = 0;
 
-
-        System.out.println(lowPulses);
-        System.out.println(highPulses);
         System.out.println("Part One: " + partOne);
         System.out.println("Part Two: " + partTwo);
 
@@ -31,31 +27,27 @@ public class day20
 
     public static int partOne(Scanner sc)
     {
-        ArrayList<ArrayList<String>> input = parseInput(sc);                                    // {{Source Module 1, Destination Module 1, ...}, ...}
+        String[][] input = parseInput(sc); // {{Source Module 1, Destination Module 1, ...}, ...}
         Object[] lookupTable = createLookupTable(input);
-        ArrayList<String[]> instructions;                                                       // {[signal Destination 1, signal, Signal Source], ...}
+        ArrayList<String[]> instructions; // {[signal Destination 1, signal, Signal Source], ...}
         // {Flip-Flop Module 1 State, ...} false = Off | true = On                          // For both of these they are at the same index as their corresponding
         // {{input Module 1, Input Module Most Recent Signal}, ...} false = Off | true = On // module in input, all other spaces are filled with null
 
-
-
         for(int i = 0; i < 1000; i++)
         {
-            //System.out.println((i+1) + ": ");
-            //System.out.println("button -low-> broadcaster");
             lowPulses++;
             instructions = findBroadcaster(input);
             while (!instructions.isEmpty())
             {
-                //System.out.println(instructions.get(0)[2] + " -"+instructions.get(0)[1]+"-> " + instructions.get(0)[0]);
-                instructions.addAll(compute(instructions.get(0), input, lookupTable));
-                instructions.remove(0);
+                instructions.addAll(compute(instructions.getFirst(), input, lookupTable));
+                instructions.removeFirst();
             }
         }
+
         return lowPulses * highPulses;
     }
 
-    public static ArrayList<ArrayList<String>> parseInput(Scanner input)
+    public static String[][] parseInput(Scanner input)
     {
         ArrayList<ArrayList<String>> result = new ArrayList<>();
 
@@ -64,45 +56,31 @@ public class day20
             ArrayList<String> currentLine = new ArrayList<>();
             currentLine.add(input.next());
             input.next();
-            currentLine.addAll(new ArrayList<>(Arrays.asList(input.nextLine().trim().split(", "))));
+            currentLine.addAll(new ArrayList<>(java.util.Arrays.asList(input.nextLine().trim().split(", "))));
             result.add(currentLine);
         }
 
-        return result;
+        return result.stream().map(u -> u.toArray(new String[0])).toArray(String[][]::new);
     }
 
-    public static ArrayList<String[]> findBroadcaster(ArrayList<ArrayList<String>> input)
-    {
-        ArrayList<String[]> result = new ArrayList<>();
-
-        for (ArrayList<String> strings : input)
-            if (strings.get(0).equals("broadcaster"))
-                for (int j = 1; j < strings.size(); j++)
-                    result.add(new String[]{strings.get(j), "low","broadcaster"});
-
-        lowPulses += result.size();
-
-        return result;
-    }
-
-    public static Object[] createLookupTable(ArrayList<ArrayList<String>> input)
+    public static Object[] createLookupTable(String[][] input)
     {
         ArrayList<Object> result = new ArrayList<>();
 
-        for (ArrayList<String> strings : input)
-            if (strings.get(0).charAt(0) == '%')
+        for (String[] strings : input)
+            if (strings[0].charAt(0) == '%')
                 result.add(false);
-            else if(strings.get(0).charAt(0) == '&')
+            else if(strings[0].charAt(0) == '&')
             {
-                String currentValue = strings.get(0).substring(1);
+                String currentValue = strings[0].substring(1);
 
                 HashMap<String, Boolean> inputPulses = new HashMap<>();
 
                 //God is dead
-                for (ArrayList<String> stringArrayList : input)
-                    for (int k = 1; k < stringArrayList.size(); k++)
-                        if (stringArrayList.get(k).equals(currentValue))
-                            inputPulses.put(stringArrayList.get(0).substring(1), false);
+                for (String[] currentLine : input)
+                    for (int k = 1; k < currentLine.length; k++)
+                        if (currentLine[k].equals(currentValue))
+                            inputPulses.put(currentLine[0].substring(1), false);
 
                 result.add(inputPulses);
             }
@@ -112,17 +90,30 @@ public class day20
         return result.toArray();
     }
 
-    public static ArrayList<String[]> compute(String[] signal, ArrayList<ArrayList<String>> input, Object[] lookupTable)
+    public static ArrayList<String[]> findBroadcaster(String[][] input)
+    {
+        ArrayList<String[]> result = new ArrayList<>();
+
+        for (String[] currentLine : input)
+            if (currentLine[0].equals("broadcaster"))
+                for (int j = 1; j < currentLine.length; j++)
+                    result.add(new String[]{currentLine[j], "low", "broadcaster"});
+
+        lowPulses += result.size();
+
+        return result;
+    }
+
+    public static ArrayList<String[]> compute(String[] signal, String[][] input, Object[] lookupTable)
     {
         int index = 0;
         boolean operator = true; // true == flip-flop, false == conjunction;
 
-        for(int i = 0; i < input.size(); i++)
-            //noinspection SequencedCollectionMethodCanBeUsed
-            if(input.get(i).get(0).substring(1).equals(signal[0]))
+        for(int i = 0; i < input.length; i++)
+            if(input[i][0].substring(1).equals(signal[0]))
             {
                 index = i;
-                operator = input.get(i).get(0).charAt(0) == '%';
+                operator = input[i][0].charAt(0) == '%';
                 break;
             }
 
@@ -132,7 +123,7 @@ public class day20
             return computeConjunction(signal, input, lookupTable, index);
     }
 
-    public static ArrayList<String[]> computeFlipFlop(String[] signal, ArrayList<ArrayList<String>> input, Object[] flipFlopStates, int index)
+    public static ArrayList<String[]> computeFlipFlop(String[] signal, String[][] input, Object[] flipFlopStates, int index)
     {
         ArrayList<String[]> result = new ArrayList<>();
 
@@ -147,8 +138,8 @@ public class day20
 
         flipFlopStates[index] = !(Boolean)flipFlopStates[index];
 
-        for(int i = 1; i < input.get(index).size(); i++)
-            result.add(new String[]{input.get(index).get(i), returnSignal, signal[0]});
+        for(int i = 1; i < input[index].length; i++)
+            result.add(new String[]{input[index][i], returnSignal, signal[0]});
 
         if(returnSignal.equals("low"))
             lowPulses += result.size();
@@ -158,7 +149,7 @@ public class day20
         return result;
     }
 
-    public static ArrayList<String[]> computeConjunction(String[] signal, ArrayList<ArrayList<String>> input, Object[] conjunctionPulses, int index)
+    public static ArrayList<String[]> computeConjunction(String[] signal, String[][] input, Object[] conjunctionPulses, int index)
     {
         ArrayList<String[]> result = new ArrayList<>();
 
@@ -171,8 +162,8 @@ public class day20
         else
             returnSignal = "high";
 
-        for(int i = 1; i < input.get(index).size(); i++)
-            result.add(new String[]{input.get(index).get(i), returnSignal, signal[0]});
+        for(int i = 1; i < input[index].length; i++)
+            result.add(new String[]{input[index][i], returnSignal, signal[0]});
 
         if(returnSignal.equals("low"))
             lowPulses += result.size();
