2c54f11e14c2aa450b3230a9af51456ffea99ea5
xestradalopez <pokelover097@gmail.com>
2024-11-23 11:28:18 -0700

    Test

--- 2023/java/day21.java
+++ 2023/java/day21.java
@@ -1,8 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class day21
 {
     static int[] sCoords = {0,0};
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        double start = System.nanoTime();
 
-    public static void main(String[] args)
+        Scanner input = new Scanner(new File("2023/input/01.txt"));
+
+        //char[][] parsedInput =
+
+        int partOne = 0;
+        int partTwo = 0;
+
+        System.out.println("Part One: " + partOne);
+        System.out.println("Part Two: " + partTwo);
+
+        double duration = (System.nanoTime() - start) / 1000000;
+        System.out.println(duration + "ms");
+    }
+
+    //public static char[][] parsedInput(Scanner input)
     {
 
     }
